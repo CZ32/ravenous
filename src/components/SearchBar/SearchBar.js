33636064e33efb05e62178552b17072bba2d06ab
CZ32 <c.zn1k96@gmail.com>
2020-07-03 18:48:02 +0100

    Handle a change in sort option

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
- 'Best Match': 'best_match',
- 'Highest Rated': 'rating',
- 'Most Reviewed': 'review_count',
-};
-
 class SearchBar extends React.Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   term: '',
+   location: '',
+   sortBy: 'best_match',
+  };
+  this.sortByOptions = {
+   'Best Match': 'best_match',
+   'Highest Rated': 'rating',
+   'Most Reviewed': 'review_count',
+  };
+ }
  renderSortByOptions() {
-  return Object.keys(sortByOptions).map((sortByOption) => {
+  return Object.keys(this.sortByOptions).map((sortByOption) => {
    const sortByOptionValue = sortByOptions[sortByOption];
    return <li key={sortByOptionValue}>{sortByOption}</li>;
   });
  }
 
+ //Retruns current CSS for a sorting Option
+ getSortedByClass(sortByOption){
+    if(this.state.sortBy == sortByOption){
+       return 'active';
+    }else{
+       return '';
+    }
+ }
+
+ // Sets state of the sorting option
+ handleSortByChange(sortByOption){
+    this.setState({sortBy: sortByOption});
+ }
+
+
  render() {
   return (
    <div className="SearchBar">
     <div className="SearchBar-sort-options">
-     <ul>
-        {this.renderSortByOptions()}
-     </ul>
+     <ul>{this.renderSortByOptions()}</ul>
     </div>
     <div className="SearchBar-fields">
      <input placeholder="Search Businesses" />
