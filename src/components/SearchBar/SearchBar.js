ee48c65319059667f680e05671590bbb3a48b576
CZ32 <c.zn1k96@gmail.com>
2020-07-05 15:26:51 +0100

    part-4 complete

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -19,6 +19,7 @@ class SearchBar extends React.Component {
   this.handleLocationChange = this.handleLocationChange.bind(this);
   this.handleTermChange = this.handleTermChange.bind(this);
   this.handleSearch = this.handleSearch.bind(this);
+  this.handleSortByChange = this.handleSortByChange.bind(this);
  }
 
  //Retruns current CSS for a sorting Option
@@ -69,8 +70,8 @@ class SearchBar extends React.Component {
      <ul>{this.renderSortByOptions()}</ul>
     </div>
     <div className="SearchBar-fields">
-     <input placeholder="Search Businesses" onClick={this.handleTermChange} />
-     <input placeholder="Where?" onClick={this.handleLocationChange} />
+     <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+     <input placeholder="Where?" onChange={this.handleLocationChange} />
     </div>
     <div className="SearchBar-submit">
      <a onClick={this.handleSearch}>Let's Go</a>
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,41 +4,33 @@ const apiKey =
 const Yelp = {
  search: function (term, location, sortBy) {
   return fetch(
-   `https://cors-anywhere.herokuapp.com/
-   https://api.yelp.com/v3/businesses/search?
-        term=${term}
-        &location=${location}
-        &sort_by=${sortBy}`,
+   `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
    {
     headers: {
      Authorization: `Bearer ${apiKey}`,
     },
    }
-  ).then(
-    (response) => {
-     if (response.ok) {
-      return response.json();
-     }
-     throw new Error('Reuest failed!');
-    },
-    (networkError) => console.log(networkError.message)
-   )
+  )
+  .then((response) => {
+    return response.json();
+   })
    .then((jsonResponse) => {
-       if(jsonResponse.businesses)
-       return jsonResponse.businesses.map(business =>{
-           return {
-               id: business.id,
-               imageSrc: business.imageSrc,
-               name: business.name,
-               address: business.address,
-               city: business.location.city,
-               state: business.location.state,
-               zipCode: business.location.zipCode,
-               category: business.category,
-               rating: business.rating,
-               review: business.review_count
-           }
-       })
+    if (jsonResponse.businesses) {
+     return jsonResponse.businesses.map((business) => ({
+      id: business.id,
+      imageSrc: business.image_url,
+      name: business.name,
+      address: business.location.address1,
+      city: business.location.city,
+      state: business.location.state,
+      zipCode: business.location.zip_code,
+      category: business.categories[0].title,
+      rating: business.rating,
+      reviewCount: business.review_count,
+     }));
+    }
    });
  },
 };
+
+export default Yelp;
